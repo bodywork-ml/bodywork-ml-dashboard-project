60bd32424a026bdfb4950f8524186a00d9d568ad
alexioannides <alex.ioanndies@yahoo.co.uk>
2021-02-23 22:35:23 +0000

    Update exception handling

--- stage-1-train-model/train_model.py
+++ stage-1-train-model/train_model.py
@@ -12,7 +12,7 @@ from datetime import date
 import boto3 as aws
 import numpy as np
 import pandas as pd
-from botocore.exceptions import ClientError
+from botocore.exceptions import ClientError, S3UploadFailedError
 from joblib import dump
 from sklearn.base import BaseEstimator
 from sklearn.linear_model import LinearRegression
@@ -81,8 +81,8 @@ def persist_dataset(dataset: pd.DataFrame, data_date: date, aws_bucket: str) -> 
             f'datasets/{dataset_filename}'
         )
         log.info(f'uploaded {dataset_filename} to s3://{aws_bucket}/datasets/')
-    except ClientError:
-        log.info('could not upload dataset to S3 - check AWS credentials')
+    except (ClientError, S3UploadFailedError):
+        log.error('could not upload dataset to S3 - check AWS credentials')
 
 
 def persist_model(model: BaseEstimator, data_date: date, aws_bucket: str) -> None:
@@ -97,8 +97,8 @@ def persist_model(model: BaseEstimator, data_date: date, aws_bucket: str) -> Non
             f'models/{model_filename}'
         )
         log.info(f'uploaded {model_filename} to s3://{aws_bucket}/models/')
-    except ClientError:
-        log.info('could not upload model to S3 - check AWS credentials')
+    except (ClientError, S3UploadFailedError):
+        log.error('could not upload model to S3 - check AWS credentials')
 
 
 if __name__ == '__main__':
