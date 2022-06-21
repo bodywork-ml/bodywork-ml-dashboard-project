80da06824082f08c5801ca5fb21fe406f32c20f3
alexioannides <alex.ioanndies@yahoo.co.uk>
2022-06-21 03:25:25 +0100

    Update train_model script

--- train_model.py
+++ train_model.py
@@ -12,8 +12,6 @@ from datetime import date
 import boto3 as aws
 import numpy as np
 import pandas as pd
-from boto3.exceptions import S3UploadFailedError
-from botocore.exceptions import ClientError
 from joblib import dump
 from sklearn.base import BaseEstimator
 from sklearn.linear_model import LinearRegression
@@ -81,7 +79,7 @@ def persist_dataset(dataset: pd.DataFrame, aws_bucket: str) -> None:
             f'datasets/{dataset_filename}'
         )
         log.info(f'uploaded {dataset_filename} to s3://{aws_bucket}/datasets/')
-    except (ClientError, S3UploadFailedError):
+    except Exception:
         log.error('could not upload dataset to S3 - check AWS credentials')
 
 
@@ -97,7 +95,7 @@ def persist_model(model: BaseEstimator, aws_bucket: str) -> None:
             f'models/{model_filename}'
         )
         log.info(f'uploaded {model_filename} to s3://{aws_bucket}/models/')
-    except (ClientError, S3UploadFailedError):
+    except Exception:
         log.error('could not upload model to S3 - check AWS credentials')
 
 
